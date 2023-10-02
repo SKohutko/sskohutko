8ada5f9c157e502860ef309735667115338bef5c
SKohutko <145993145+SKohutko@users.noreply.github.com>
2023-10-02 15:55:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,41 +11,66 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 let todos = [];
 
+function loadTodos() {
+  const storedTodos = localStorage.getItem('todos');
+  return storedTodos ? JSON.parse(storedTodos) : [];
+}
+
+function saveTodos() {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function updateTodos() {
+  render();
+  saveTodos();
+}
+
 function newTodo() {
-    let text = window.prompt("Введіть справу");
-    let todo = { id: Date.now(), text, checked: false };
+  const text = window.prompt("Введіть нову справу");
+  if (text) {
+    const todo = { id: numb++, text, checked: false };
     todos.push(todo);
-    render();
-}
-
-function render() {
-    list.innerHTML = todos.map(todo => renderTodo(todo)).join('');
-    itemCountSpan.innerHTML = todos.length;
-    uncheckedCountSpan.innerHTML = todos.filter(todo => !todo.checked).length;
-}
-
-function renderTodo(todo) {
-    return `<div class="${classNames.TODO_ITEM}">
-        <input class="${classNames.TODO_CHECKBOX}" type="checkbox" ${todo.checked ? "checked" : ""} onchange="toggleTodo(${todo.id})">
-        <span class="${classNames.TODO_TEXT}">${todo.text}</span>
-        <button class="${classNames.TODO_DELETE}" onclick="deleteTodo(${todo.id})">Видалити</button>
-    </div>`;
+    updateTodos();
+  }
 }
 
 function deleteTodo(id) {
-    todos = todos.filter(todo => todo.id !== id);
-    render();
+  todos = todos.filter(todo => todo.id !== id);
+  updateTodos();
 }
 
 function toggleTodo(id) {
-    todos = todos.map(todo =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo
-    );
-    render();
+  todos = todos.map(todo =>
+    todo.id === id ? { ...todo, checked: !todo.checked } : todo
+  );
+  updateTodos();
 }
 
-render();
+const list = document.getElementById('todo-list');
+const itemCountSpan = document.getElementById('item-count');
+const uncheckedCountSpan = document.getElementById('unchecked-count');
 
+let todos = [];
+let numb = 1;
+
+window.onload = () => {
+  todos = loadTodos();
+  render();
+};
+
+function renderTodo(todo) {
+  return `<li>
+    <input type="checkbox" ${todo.checked ? "checked" : ""} onchange="toggleTodo(${todo.id})">
+    <span>${todo.text}</span>
+    <button onclick="deleteTodo(${todo.id})">Видалити</button>
+  </li>`;
+}
+
+function render() {
+  list.innerHTML = todos.map(todo => renderTodo(todo)).join('');
+  itemCountSpan.innerText = todos.length;
+  uncheckedCountSpan.innerText = todos.filter(todo => !todo.checked).length;
+}
 
 
 
